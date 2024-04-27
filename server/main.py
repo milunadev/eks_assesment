60d72262a5d3b9956d0901e4c70f1d839dc4f764
milunadev <misusuyatec@gmail.com>
2024-04-26 21:53:08 -0500

    APPLY NEW DEPLOYMENTT SERVER

--- server/main.py
+++ server/main.py
@@ -10,6 +10,13 @@ secret_key = 'mydemokey'
 database = "demo"
 auth_collection = "users"
 
+users = {
+        'admin': 'pass',
+        'mimi': 'pass',
+        'pepe': 'pass',
+        'juan': 'pass'
+    }
+
 class AuthService(rpc.AuthServiceServicer):
 
     def __init__(self,mongo_url,jwt_secret):
@@ -22,18 +29,25 @@ class AuthService(rpc.AuthServiceServicer):
     def AuthenticateUser(self, request, context):
         print("Authenticating user: ", request.username)
         username = request.username
-        password = request.password 
+        password = request.password
 
-        #Buscar user en mongo
-        user = self.users_collection.find_one({'username': username, 'password': password})
-        print("User found: ", user)
-        if user:
-            print("User found: ", user, " generating token")
+        if username in users and users[username] == password:
+            print("User found: ", username, " generating token")
             token = jwt.encode({'username': username}, self.jwt_secret , algorithm='HS256')
             return chat.LoginResponse(token=token)
         else:
             context.abort(grpc.StatusCode.UNAUTHENTICATED, 'Invalid username or password')
-            #return chat.LoginResponse(error='Invalid username or password')
+
+        # #Buscar user en mongo
+        # user = self.users_collection.find_one({'username': username, 'password': password})
+        # print("User found: ", user)
+        # if user:
+        #     print("User found: ", user, " generating token")
+        #     token = jwt.encode({'username': username}, self.jwt_secret , algorithm='HS256')
+        #     return chat.LoginResponse(token=token)
+        # else:
+        #     context.abort(grpc.StatusCode.UNAUTHENTICATED, 'Invalid username or password')
+        #     #return chat.LoginResponse(error='Invalid username or password')
 
 
 class ChatServer(rpc.ChatServerServicer):
@@ -41,7 +55,7 @@ class ChatServer(rpc.ChatServerServicer):
     def __init__(self):
         #History of chat messages
         self.chats = []
-    
+
     #Streaming method from server to clients
     def ChatStream(self, request_iterator, context):
         last_index = 0
@@ -51,13 +65,13 @@ class ChatServer(rpc.ChatServerServicer):
                 msg = self.chats[last_index]
                 last_index += 1
                 yield msg #Send mesage to clients
-    
+
     #Method to receive messages from clients
     def SendNote(self, request, context):
         print(f'{request.name}: {request.message}')
         self.chats.append(request)   #Append to message history
         return chat.EmptyMessage()   #Return empty message
-    
+
     def SaveChat(self, request_iterator, context):
         print('Iniciando guardado de chat...', self)
         try:
@@ -75,7 +89,7 @@ class ChatServer(rpc.ChatServerServicer):
         except Exception as e:
             print(f'Error durante el guardado de chat: {e}')
             return chat.SaveChatResponse(success=False)
-    
+
 def serve():
     jwt_secret = secret_key
     mongo_url = 'mongodb://localhost:27017'
@@ -83,8 +97,8 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     rpc.add_ChatServerServicer_to_server(ChatServer(), server)
     rpc.add_AuthServiceServicer_to_server(AuthService(mongo_url,jwt_secret), server)
-   
-    
+
+
     server.add_insecure_port('[::]:50051')
     server.start()
     print('🚀 Servidor iniciado, escuchando...')
@@ -94,7 +108,7 @@ def serve():
         print('⌨️ Interrupción por teclado detectada, cerrando el servidor...')
         server.stop(0)  # Detiene el servidor de inmediato
         print('✅ Servidor cerrado correctamente.')
-        
+
 
 if __name__ == '__main__':
     serve()
